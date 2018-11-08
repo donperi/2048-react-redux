70cdc64f80aad392d3a66be3af1b60e963fe3473
Briam Santiago <bm.santiago14@gmail.com>
2018-11-08 13:33:44 -0400

    Add move animation

--- src/components/BoardCell.js
+++ src/components/BoardCell.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import classNames from 'classnames';
-import TweenLite from 'gsap';
+import { TweenLite, TimelineLite } from 'gsap';
 
 import './BoardCell.scss';
 
@@ -8,11 +8,17 @@ function BoardCell({ rowIndex, cellIndex, data }) {
   const cellRef = useRef();
   const [ cellHeight, setCellHeight ] = useState(null)
   const cell = data.board[rowIndex][cellIndex];
+  const positionProps = {
+    "UP": "top",
+    "DOWN": "bottom",
+    "RIGHT": "right",
+    "LEFT": "left"
+  }
 
   useEffect(() => {
     function handleResize() {
       if (cellRef.current) {
-        setCellHeight(`${cellRef.current.offsetWidth}px`);
+        setCellHeight(`${cellRef.current.offsetWidth}`);
       }  
     }
 
@@ -24,15 +30,31 @@ function BoardCell({ rowIndex, cellIndex, data }) {
     };
   }, [cellHeight])
 
-  useEffect(() => {
-    if (cell.merged) {
-      TweenLite.to(cellRef.current, 0.3, { transform: 'scale(1)' });
-    }
-    
+  useEffect(() => {  
     if (cell.is_new) {
       TweenLite.to(cellRef.current, 0.3, { transform: 'scale(1)' });
     }
+
+    if (cell.moved_pos > 0) {
+      const timeline = new TimelineLite();
+
+
+
+      timeline.add(TweenLite.to(cellRef.current, 0.3, { [positionProps[data.last_move]]: '0px' }));
+      
+      if (cell.merged) {
+        timeline.add(TweenLite.to(cellRef.current, 0.3, { transform: 'scale(1)' }), "-=0.1");
+      }
+      
+      timeline.play();
+    }
   });
+
+  let position = null;
+  if (cell.moved_pos > 0) {
+    position = cellHeight * cell.moved_pos + (cell.moved_pos * 4)
+    cellRef.current.style[positionProps[data.last_move]] = `${position}px`
+  }
 
   const classes = classNames({
     'merged': cell.merged,
@@ -43,8 +65,9 @@ function BoardCell({ rowIndex, cellIndex, data }) {
     <div 
       key={JSON.stringify(cell)}
       ref={cellRef} 
-      style={{ height: cellHeight }} 
+      style={{ height: `${cellHeight}px` }} 
       className={`BoardCell number-${cell.value} ${classes}`}
+      data-cell={JSON.stringify(cell)}
     >
     <span className={`BoardCell-value`}>
       {cell.value !== 0 ? cell.value : null }
